3186859e4891fe149cc3e2aa82d0a2e991eb70b8
sanSkiller <1290148953@qq.com>
2019-10-11 21:54:16 +0800

    improve the login module

--- router/user.js
+++ router/user.js
@@ -2,20 +2,29 @@ const router = require("koa-router")()
 const db = require("../lib/mysql")
 
 router.get("/", async ctx=>{
-  ctx.body = "hello, i'm user log page"
+  let res
+  if (ctx.session.login !== "1") {
+    res = {result: 0}
+  } else {
+    res = {result: 1}
+  }
+
+  ctx.body = res
 })
 
 // 登录处理
 router.post("/login", async ctx=>{
   let {user, password} = ctx.request.body
   
-  let res = await db.cSelect(["uID", "user", "password"], "users", {user, password})
+  let res = await db.cSelect("*", "users", {user, password})
 
   let body
-
+  // 添加登陆成功的状态
+  
   if (res != false) {
+    res[0]["login"] = 1
     ctx.session.login = "1"
-    body = {result: 1, message: "登陆成功，正在跳转..."}
+    body = {result: 1, message: "登陆成功，正在跳转...", data: res[0]}
   } else {
     body = {result: 0, message: "账号或密码错误"}
   }
